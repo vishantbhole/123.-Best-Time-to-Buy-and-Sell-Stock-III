954d4e9cfaeeb6ca9f1c781cd8bd0935318ee972
Vishant <44126013+vishantbhole@users.noreply.github.com>
2026-02-17 20:11:01 -0600

    Update maxProfit3.py

--- maxProfit3.py
+++ maxProfit3.py
@@ -14,3 +14,8 @@ class Solution:
         for t in range(1,2+1):
             pos = -prices[0]
             profit = 0
+            for i in range(1, len(prices)):
+                pos = max(pos, dp[i]-prices[i])
+                profit = max(profit, pos+prices[i])
+                dp[i] = profit
+        return profit
